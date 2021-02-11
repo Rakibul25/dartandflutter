97e858372db11b8042aba60d61cf63fd691b6695
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-02-12 01:49:31 +0600

    basic widget practice, commit 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,29 @@
 import 'package:flutter/material.dart';
 
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Dart & flutter1",
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Dart & Flutter"
+          ),
+        ),
+        body: Center(
+          child: Text(
+            "Hello from centre!"
+          ),
+        )
+      ),
+
+    );
+  }
+}
