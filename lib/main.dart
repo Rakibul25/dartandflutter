70a3142e25d601ba2d978b988d286d8eec78a863
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-02-17 15:23:30 +0600

    asset and image

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,14 @@
 import 'package:flutter/material.dart';
-
-void main() {
+void main(){
   runApp(MyApp());
 }
-
-class MyApp extends StatefulWidget {
-  String name = "";
-  String textfieldvalue = "";
-  void showtext(){
-
-  }
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Dart & flutter1",
-      theme: ThemeData(
-        primarySwatch: Colors.red,
+    return Container(
+      child: Center(
+        child: Image.asset("assets/images/cool.png"),
       ),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              "Dart & Flutter",
-            ),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: TextField(
-                    onChanged: (text){
-                      name = text;
-                    },
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.account_box),
-                        labelText: "Input here"),
-                    onChanged: (text){
-                      textfieldvalue = text;
-                    },
-                    maxLength: 10,
-                    style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-                RaisedButton(
-                  onPressed: showtext,
-                  child: Text(
-                    "press"
-                  ),
-                )
-              ],
-            ),
-          )),
     );
-  }
-
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
   }
 }
