a3c4452a6f5a32c704b86a6063e5104d785bfee5
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-02-18 20:31:54 +0600

    textfield value fetch and button on pressed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,71 @@
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Image.asset("assets/images/cool.png"),
+    return MaterialApp(
+      title: 'Dart and Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String value = "";
+  String texttoshow = "";
+
+  void showtextup(){
+    setState(() {
+      texttoshow = value;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "$texttoshow"
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: (text){
+                value = text;
+              },
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.account_box),
+                  hintText: "Input here",
+                  border: OutlineInputBorder(),
+                  labelText: "Input here"),
+              maxLength: 10,
+              style: TextStyle(
+                fontSize: 19,
+                color: Colors.red,
+              ),
+            ),
+          ),
+          RaisedButton(
+            onPressed: showtextup,
+            child: Text(
+              "click here"
+            ),
+          )
+        ],
       ),
     );
   }
